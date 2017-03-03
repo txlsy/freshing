fbb9d15f9b83041af80d47d57fce553368cf52d7
李思渊 <334224109@qq.com>
2017-03-03 13:05:57 +0800

    新建项目，部署成功

---
+++ src/main/java/com/mark/website/filter/EncodingFilter.java
@@ -0,0 +1,27 @@
+package com.mark.website.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+
+	private String charset = null;
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+		req.setCharacterEncoding(charset);
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		charset = config.getInitParameter("CharSet");
+		if(null == charset) charset = "UTF-8";
+	}
+
+}
