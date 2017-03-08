68282353332ca56bdae332e11388bc0540a2c5ef
txlsy <334224109@qq.com>
2017-03-08 14:31:42 +0800

    管理首页

--- src/main/java/com/mark/website/servlet/admin/AdminIndexServlet.java
+++ src/main/java/com/mark/website/servlet/admin/AdminIndexServlet.java
@@ -1,38 +1,20 @@
 package com.mark.website.servlet.admin;
 
-import com.mark.website.bean.UserBean;
-import com.mark.website.model.admin.Administrator;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Writer;
 
 /**
- * @author mark on 2017/3/6.
+ * @author mark on 2017/3/7.
  */
 public class AdminIndexServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
 
-        Administrator administrator = new Administrator();
-        administrator.setName(username);
-        administrator.setPassword(password);
-
-        UserBean userBean = new UserBean();
-        Boolean r = userBean.checkAdmin(administrator);
-
-        Writer writer = response.getWriter();
-        if(r) writer.write(this.getInitParameter("admin-index"));
-        else writer.write(r.toString());
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/template/admin/admin.html").include(request, response);
+        request.getRequestDispatcher("/template/admin/index.html").include(request, response);
     }
-
 }
